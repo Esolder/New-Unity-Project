c17ce25ae1074baff283927e0813209fb41650c2
feelleen <39160506+feelleen@users.noreply.github.com>
2018-05-31 00:05:40 +0300

    first

---
+++ New Unity Project/Assets/MenuControlsScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControlsScript : MonoBehaviour
+{
+    
+
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("Test001");
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+        Debug.Log("I've closed the game!");
+    }
+
+}
